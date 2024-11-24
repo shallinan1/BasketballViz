82b6dffbdd914ce82a6b1ba3c7c25c5b9d108da2
Skyler Hallinan <hallisky@uw.edu>
2024-11-23 17:02:27 -0800

    Lance + Gavin

---
+++ players.js
@@ -0,0 +1,70 @@
+// players.js
+export const players = [
+
+    {
+        number: 4,
+        name: "Isaac Araki",
+        team: "Unknown",
+        position: "Big",
+        height: "6'3\"",
+        weight: "220lbs",
+        age: 22,
+        season: 3,
+        image: "images/isaac.png", // Placeholder image URL
+        description: "A raw, floor-stretching big with boundless hustle, still finding his footing as he sharpens his defense and polishes his game.",
+        icons: ["Spacer", "Ridiculous Upside"]
+    },
+    {
+        number: 1,
+        name: "Paul Rosiak",
+        team: "Milwaukee Bucks",
+        position: "Wing",
+        height: "6'0\"",
+        weight: "175 lbs",
+        age: 33,
+        season: 5,
+        image: "https://via.placeholder.com/150", // Placeholder image URL
+        description: "An on-ball menace with elite instincts for steals, paired with smooth, fundamental offense which prizes efficiency over flair.",
+        icons: ["Positional Versatility", "Finisher", "On-Ball Defense"]
+    },
+    {
+        number: 3,
+        name: "Mingma Sherpa",
+        team: "Los Angeles Lakers",
+        position: "Forward",
+        height: "5'9\"",
+        weight: "180 lbs",
+        age: 24,
+        season: 4,
+        image: "images/ming.png", // Placeholder image URL
+        description: "Elite rebounder with a knack for bold, clever passes that keep defenses guessing -- and teammates on their toes.",
+        icons: ["Rebounding", "Positional Versatility", "Passing Virtuoso", ]
+    },
+    {
+        number: 3,
+        name: "Lance Muchina",
+        team: "Los Angeles Lakers",
+        position: "Guard",
+        height: "5'5\"",
+        weight: "150 lbs",
+        age: 24,
+        season: 4,
+        image: "images/minpng", // Placeholder image URL
+        description: "Relentless on-ball defender with grit, heart, and a team-first mentality, occasionally flashing a serviceable three-point touch.",
+        icons: ["On-Ball Defense", "Hustle"]
+    },
+    {
+        number: 2,
+        name: "Gavin Pereria",
+        team: "Golden State Warriors",
+        position: "Guard",
+        height: "6'2\"",
+        weight: "175 lbs",
+        age: 24,
+        season: 4,
+        image: "images/minpng", // Placeholder image URL
+        description: "A crafty playmaker sporting a silky crossover, with a knack for defending the perimeter with his impressive reach.",
+        icons: ["Passing Virtuoso", "Positional Versatility", "Shot Blocker"]
+    },
+    
+];
